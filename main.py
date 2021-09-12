b66a8fbc38ea0a0de8d972e9faf71611ef2c27dc
ilya <test@text.com>
2021-09-12 16:10:41 +0500

    update

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from PyPDF2 import PyPDF2
 
-new_pdf = PyPDF2.PdfFileReader("1.pdf")
-print(new_pdf.getDocumentInfo())
-print(new_pdf.getNumPages())
 
+with open('1.pdf', 'rb') as pdf_file: 
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file) 
+    # printing first page contents
+    pdf_page = pdf_reader.getPage(0) 
+    print(pdf_page.extractText()) # reading all the pages content one by one
+    for page_num in range(pdf_reader.numPages):
+        pdf_page = pdf_reader.getPage(page_num) 
+        print(pdf_page.extractText())
